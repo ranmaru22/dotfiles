e1a763cea87f3db708d32fa39cd471b5deb4be08
Alex Sun <alexsun82@icloud.com>
2020-10-25 00:44:25 +0200

    Remove git aliases Now stored in gitconfig

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -35,61 +35,20 @@ confedit() {
 } # }}}
 alias ce=confedit
 
-# Git aliases {{{
-alias gp="git push"
-alias gf="git fetch"
-alias gc="git clone"
-alias gb="git branch"
-alias gm="git merge"
-alias gip="git pull"
-
-# add
-alias gia="git add"
-alias gaa="git add -A"
-alias gap="git add -p"
-
-# checkout
-alias gco="git checkout"
-alias gcb="git checkout -b"
-alias gcom="git checkout master"
-
-# status, diff, etc.
-alias gds="git status"
-alias gdi="git diff"
-alias gdc="git diff --cached"
-alias glog="git log --oneline --decorate --graph"
-
-# stashing
-alias gst="git stash"
-alias gsp="git stash pop"
-
-# cherry picking
-alias gcp="git cherry-pick"
-alias gcpa="git cherry-pick --abort"
-alias gcpc="git cherry-pick --continue"
-
-# committing
-alias gcm="git commit -m"
-alias gcA="git commit -a --amend"
-
-# rebasing
-alias grb="git rebase"
-alias gri="git rebase -i"
-
-# uncommitting
-guc() {
-  git reset @~ "$@" && git commit --amend --no-edit
-}
-
-# git fuzzy
-alias gzf="git fuzzy"
-alias gzs="git fuzzy status"
-alias gzb="git fuzzy branch"
-alias gzl="git fuzzy log"
-alias gzr="git fuzzy reflog"
-alias gzst="git fuzzy stash"
-alias gzd="git fuzzy diff"
-alias gzp="git fuzzy pr"
-# }}}
+# Git wrapper {{{
+git() {
+  if [ "$1" = "root" ]; then
+    shift
+    local ROOT="$(command git rev-parse --show-toplevel 2> /dev/null || echo -n .)"
+    if [ $# -eq 0 ]; then
+      cd "$ROOT"
+    else
+      (cd "$ROOT" && eval "$@")
+    fi
+  else
+    command git "$@"
+  fi
+} # }}}
+alias g=git
 
 # vim:foldmethod=marker:foldlevel=0
